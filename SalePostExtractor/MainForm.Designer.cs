5fb60d86f2bcecaebe9e3298647c33c88b0ca147
rajeem\ <rajeem_cariazo@yahoo.com>
2016-04-04 03:07:56 +0800

    Initial Commit

---
+++ SalePostExtractor/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SalePostScraper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnViewSource = new System.Windows.Forms.Button();
+            this.btnNavigate = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.rtbSource = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(865, 414);
+            this.browser.TabIndex = 0;
+            this.browser.Url = new System.Uri("https://www.facebook.com/groups/baguiobuyandsell/forsaleposts", System.UriKind.Absolute);
+            this.browser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.browser_Navigated);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnNavigate);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 414);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(865, 49);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnViewSource
+            // 
+            this.btnViewSource.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnViewSource.Location = new System.Drawing.Point(0, 391);
+            this.btnViewSource.Name = "btnViewSource";
+            this.btnViewSource.Size = new System.Drawing.Size(200, 23);
+            this.btnViewSource.TabIndex = 6;
+            this.btnViewSource.Text = "View Source";
+            this.btnViewSource.UseVisualStyleBackColor = true;
+            this.btnViewSource.Click += new System.EventHandler(this.btnViewSource_Click);
+            // 
+            // btnNavigate
+            // 
+            this.btnNavigate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNavigate.Location = new System.Drawing.Point(778, 11);
+            this.btnNavigate.Name = "btnNavigate";
+            this.btnNavigate.Size = new System.Drawing.Size(75, 23);
+            this.btnNavigate.TabIndex = 3;
+            this.btnNavigate.Text = "Go";
+            this.btnNavigate.UseVisualStyleBackColor = true;
+            this.btnNavigate.Click += new System.EventHandler(this.btnNavigate_Click);
+            // 
+            // textBox1
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(100, 14);
+            this.txtUrl.Name = "textBox1";
+            this.txtUrl.Size = new System.Drawing.Size(672, 20);
+            this.txtUrl.TabIndex = 2;
+            // 
+            // rtbSource
+            // 
+            this.rtbSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbSource.Location = new System.Drawing.Point(0, 0);
+            this.rtbSource.Name = "rtbSource";
+            this.rtbSource.Size = new System.Drawing.Size(200, 414);
+            this.rtbSource.TabIndex = 3;
+            this.rtbSource.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnViewSource);
+            this.panel2.Controls.Add(this.rtbSource);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(665, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 414);
+            this.panel2.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 463);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Facebook Extractor";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNavigate;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnViewSource;
+        private System.Windows.Forms.RichTextBox rtbSource;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ SalePostExtractor/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+
+namespace SalePostScraper
+{
+    public partial class MainForm : Form
+    {
+        List<string> list;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            list = new List<string>();
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            var defaultLoc = "baguio city";
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(browser.DocumentText);
+            var salePosts = doc.DocumentNode.SelectNodes("//*[contains(@class,'_5pcb')]//a");
+            foreach (var item in salePosts)
+            {
+                var url = item.GetAttributeValue("href", "");
+                list.Add(url);
+            }
+            foreach (var item in list)
+            {
+                browser.Navigate("https://www.facebook.com/" + item);
+
+                //todo: optimize(not a good solution)
+                while (browser.ReadyState != WebBrowserReadyState.Complete)
+                {
+                    Application.DoEvents();
+                }
+                doc.LoadHtml(browser.DocumentText);
+                var scope = doc.DocumentNode.SelectSingleNode("//*[contains(@class,'userContentWrapper')]");
+
+                var title = scope.SelectNodes("./div[1]/div[5]/div[1]/div[1]/div[1]/div[1]")[0].InnerText;
+                var price = scope.SelectNodes("./div[1]/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]")[0].InnerText;
+                var xpathLoc = "./div[1]/div[5]/div[1]/div[1]/div[1]/div[2]/div[2]";
+                var location = scope.SelectNodes(xpathLoc) != null ? scope.SelectNodes(xpathLoc)[0].InnerText : defaultLoc;
+                var body = scope.SelectNodes("./div[1]/div[5]/div[1]/div[1]/div[1]/div[3]")[0].InnerText;
+
+                var userRaw = scope.SelectNodes("./div[1]/div[3]//a")[1].Attributes["href"].Value;
+                var userId = Regex.Match(userRaw, @"\?id=([^&]*)").ToString().Replace("?id=", "");
+
+                var images = GetImages();
+
+                MessageBox.Show("USER: " + userId + "\nTITLE: " + title + "\nPRICE: " + price + "\nLOCATION: " + location + "\nBODY: " + body);
+            }
+        }
+
+        private IEnumerable<Bitmap> GetImages()
+        {
+            IHTMLDocument2 doc2 = (IHTMLDocument2)browser.Document.DomDocument;
+            IHTMLControlRange imgRange = (IHTMLControlRange)((HTMLBody)doc2.body).createControlRange();
+
+            var list = new List<Bitmap>();
+            foreach (IHTMLImgElement img in doc2.images)
+            {
+                if (img.src.StartsWith("https://fbcdn-photos"))
+                {
+                    imgRange.add((IHTMLControlElement)img);
+                    imgRange.execCommand("Copy", false, null);
+                    using (Bitmap bmp = (Bitmap)Clipboard.GetDataObject().GetData(DataFormats.Bitmap))
+                    {
+                        list.Add(bmp);
+                    }
+                }
+            }
+            return list;
+        }
+
+        private void btnNavigate_Click(object sender, EventArgs e)
+        {
+            browser.Navigate(txtUrl.Text);
+        }
+
+        private void btnViewSource_Click(object sender, EventArgs e)
+        {
+            rtbSource.Text = browser.DocumentText;
+        }
+
+        private void browser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            txtUrl.Text = browser.Url.ToString();
+        }
+    }
+}
